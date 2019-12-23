95d3989d0ceb5e33ed0b6ae64b2c29342d4c4675
Ramesh Kumar <ramesh@primority.com>
2019-12-23 16:55:50 +0530

    sdsd

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const express = require('express');
 const app = express();
 //sjhjs
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept,Authorization,Cache-Control',
+    );
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');
+    next();
+  });
+
 app.get("/user/mamahome", (req, res) => {
     var test = [1, 2, 3, 4, 5, 6, 7]
     res.send({ address: test });
