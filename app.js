c8d236efda8ded94ae5aab701d42c2dcf22fabfa
Ramesh Kumar <ramesh@primority.com>
2019-12-23 12:16:34 +0530

    firstCommit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/user/mamahome", (req, res) => {
+    var test = [1, 2, 3, 4, 5, 6, 7]
+    res.send({ address: test });
+});
+
+app.get("/order/mamahome", (req, res) => {
+    var test = ["A","B","C"]
+    res.send({ address: test });
+});
+
+app.listen(3000, () => {
+    console.log('society exists');
+})
